80ea61ff0e01637cbb8e304e6723f08d56ebe1cf
simonebarberini <simonebarberini02@gmail.com>
2024-12-19 22:52:19 +0100

    initialCommit

---
+++ src/test/java/com/gestionalePensione/GestionalePensioneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionalePensione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionalePensioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
